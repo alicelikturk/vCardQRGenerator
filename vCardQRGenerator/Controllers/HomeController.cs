d59657d97e50bf65a8c7bdf4bb33d8619a5e5a7e
Ali CELIKTURK <ekber038@gmail.com>
2023-06-01 17:49:15 +0300

    released

--- vCardQRGenerator/Controllers/HomeController.cs
+++ vCardQRGenerator/Controllers/HomeController.cs
@@ -45,13 +45,14 @@ public class HomeController : Controller
     [HttpPost("read-csv-file")]
     public async Task<IActionResult> GetCSVFile([FromForm] IFormFileCollection file)
     {
+        Console.WriteLine();
         var vcards = _csvService.ReadCSV<Vcard>(file[0].OpenReadStream());
 
         return Ok(vcards);
     }
 
     [HttpPost("generate-qr")]
-    public async Task<IActionResult> GetQrCode(string vCardInfo)
+    public async Task<IActionResult> GetQrCode([FromForm] string vCardInfo, IFormFile file)
     {
         Byte[] byteArray;
         var width = 250; // width of the QR Code
@@ -69,11 +70,23 @@ public class HomeController : Controller
         };
         var pixelData = qrCodeWriter.Write(vCardInfo.Replace("%0A", "\n"));
 
-
-       
-
         using var image = Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Bgra32>(pixelData.Pixels, pixelData.Width, pixelData.Height);
 
+        if (file != null)
+        {
+
+            using var imageLogo = Image.Load(file.OpenReadStream());
+            string newSize = ResizeImage(imageLogo, 50, 50);
+            string[] aSize = newSize.Split(',');
+            imageLogo.Mutate(h => h.Resize(Convert.ToInt32(aSize[1]), Convert.ToInt32(aSize[0])));
+
+            image.Mutate(imageContext =>
+            {
+                // new Point((image.Width - imageLogo.Width) / 2, (image.Height - imageLogo.Height) / 2)
+                imageContext.DrawImage(imageLogo, new Point((image.Width - imageLogo.Width) / 2, (image.Height - imageLogo.Height)/2), 1);
+            });
+
+        }
 
         using MemoryStream ms = new MemoryStream();
         image.SaveAsPng(ms);
@@ -81,12 +94,50 @@ public class HomeController : Controller
         //file.CopyTo(ms);
         //fileArray = Encoding.ASCII.GetString(ms.ToArray());
         //fileArray = BitConverter.ToString(ms.ToArray()).Replace("-", "");
-        Console.WriteLine(vCardInfo);
         return Ok(String.Format("data:image/png;base64,{0}", Convert.ToBase64String(ms.ToArray())));
 
 
 
 
+    }
+
+    private byte[] UploadedFile(IFormFile file)
+    {
+        var fileArray = new byte[] { };
+
+        if (file != null)
+        {
+            var image = Image.Load(file.OpenReadStream());
+            string newSize = ResizeImage(image, 30, 30);
+            string[] aSize = newSize.Split(',');
+            image.Mutate(h => h.Resize(Convert.ToInt32(aSize[1]), Convert.ToInt32(aSize[0])));
+
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, new JpegEncoder());
+
+                //file.CopyTo(ms);
+                //fileArray = Encoding.ASCII.GetString(ms.ToArray());
+                //fileArray = BitConverter.ToString(ms.ToArray()).Replace("-", "");
+                fileArray = ms.ToArray();
+            }
+        }
+        return fileArray;
+    }
+    private string ResizeImage(Image img, int maxWidth, int maxHeight)
+    {
+        if (img.Width > maxWidth || img.Height > maxHeight)
+        {
+            double widthRatio = (double)img.Width / (double)maxWidth;
+            double heightRatio = (double)img.Height / (double)maxHeight;
+            double ratio = Math.Max(widthRatio, heightRatio);
+            int newWidth = (int)(img.Width / ratio);
+            int newHeight = (int)(img.Height / ratio);
+            return newHeight.ToString() + "," + newWidth.ToString();
+        }
+        else
+            return img.Height.ToString() + "," + img.Width.ToString();
     }
 
 
--- vCardQRGenerator/Models/Vcard.cs
+++ vCardQRGenerator/Models/Vcard.cs
@@ -1,12 +1,118 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+using CsvHelper.Configuration.Attributes;
+
 namespace vCardQRGenerator.Models
 {
 	public class Vcard
 	{
-		public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        [Optional]
+        [JsonPropertyName("firstname")]
+        public string FirstName { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("lastname")]
+        public string LastName { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("birthday")]
+        public string? Birthday { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("gender")]
+        public string? Gender { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("homestreet")]
+        public string? HomeStreet { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("homecity")]
+        public string? HomeCity { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("homeregion")]
+        public string? HomeRegion { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("homepost")]
+        public string? HomePost { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("homecountry")]
+        public string? HomeCountry { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("hometel")]
+        public string? HomeTel { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("homeemail")]
+        public string? HomeEmail { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("homeurl")]
+        public string? HomeUrl { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgtitle")]
+        public string? OrgTitle { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgname")]
+        public string? OrgName { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgstreet")]
+        public string? OrgStreet { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgcity")]
+        public string? OrgCity { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgregion")]
+        public string? OrgRegion { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgpost")]
+        public string? OrgPost { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgcountry")]
+        public string? OrgCountry { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgtel")]
+        public string? OrgTel { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgemail")]
+        public string? OrgEmail { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("orgurl")]
+        public string? OrgUrl { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("facebook")]
+        public string? Facebook { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("twitter")]
+        public string? Twitter { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("flickr")]
+        public string? Flickr { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("youtube")]
+        public string? Youtube { get; set; } = "";
+
+        [Optional]
+        [JsonPropertyName("skype")]
+        public string? Skype { get; set; } = "";
     }
 }
 
